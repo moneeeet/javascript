1d105286e893588d8e141fb82a4282e991d3f49b
moneeeet <t.o.p11040809@gmail.com>
2022-09-17 08:50:46 +0000

    関数

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-let max = 100;
-let num = 1;
-let count = 0;
-
-while(num < max){
-  num = num * 2;
-  count = count + 1;
+function addString(strA){
+  let addStr = "Hello " + strA;
+  return addStr;
 }
-
-alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
